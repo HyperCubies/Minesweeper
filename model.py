42614e1649b5d1684e088050fb68e16ad7065e09
xush <xush@shanghaitech.edu.cn>
2018-02-19 14:41:12 +0800

    ha

--- model.py
+++ model.py
@@ -1,55 +1,99 @@
 from random import sample
 
+class Block:
+	def __init__(self):
+		self._mine = False
+		self._revived = False
+		self._flaged = False
+		self._rounding = 0
+	def getmine(self):
+		return self._mine
+	def setmine(self):
+		self._mine = True
+	def getrounding(self):
+		return self._rounding
+	def setrounding(self, num):
+		self._rounding = num
+	def getflag(self):
+		return self._flaged
+	def setflag(self):
+		self._flaged = not self._flaged
+	def revived(self):
+		return self._revived
+	def revive(self):
+		self._revived = True
+
 class Minesmap:
 	def __init__(self, width, height, number_of_mines):
 		self.height = height
 		self.width = width
 		seed = sample(range(width * height), number_of_mines)
-		minesmap = [[0 for j in range(height)] for i in range(width)]
+		minesmap = tuple(tuple(Block() for j in range(height)) for i in range(width))
 		for num in seed:
-			minesmap[num//height][num%height] = 1
-		self._minesmap = tuple(tuple(sublist) for sublist in minesmap)
-		self._revived = [[0 for j in range(height)] for i in range(width)]
-		self._flag = [[0 for j in range(height)] for i in range(width)]
+			minesmap[num//height][num%height].setmine()
+		for x in range(width):
+			for y in range(height):
+				rounding = 0
+				for i, j in self.around(x, y):
+					rounding += minesmap[i][j].getmine()
+				minesmap[x][y].setrounding(rounding)
+		self._map = minesmap
+
+	def __getitem__(self, index):
+		x, y = index
+		return self._map[x][y]
 
 	def around(self, x, y):
-		return [(i, j) for j in range(max(0, y - 1),min(self.height, y + 2)) for i in range(max(0, x - 1),min(self.width, x + 2))]
+		return [(i, j) for j in range(max(0, y - 1),min(self.height, y + 2)) for i in range(max(0, x - 1),min(self.width, x + 2)) if i != x or j != y]
 
 	def getnumber(self, x, y):
-		around = 0
-		for i, j in self.around(x, y):
-			if self._minesmap[i][j]:
-				around += 1
-		return around
+		return self._map[x][y].getrounding()
 
 	def revive(self, x, y):
-		self._revived[x][y] = 1
-		if self._minesmap[x][y]:
-			return True
-		if self.getnumber(x, y):
-			return False
-		else:
+		block = self._map[x][y]
+		if block.revived():
+			return
+		block.revive()
+		if not block.getrounding():
 			for i, j in self.around(x, y):
-				if not self._revived[i][j]:
-					Minesmap.revive(self, i, j)
-			return False
+				self.revive(i, j)
 
 	def setflag(self, x, y):
-		self._flag[x][y] = 1
+		self._map[x][y].setflag()
 
 	def flagrevive(self, x, y):
 		for i, j in self.around(x, y):
-			if not self._flag[i][j]:
-				self.revive[i][j]
+			if not self._map[i][j].getflag():
+				self.revive(i, j)
 
 	def outer(self):
+		s = ''
 		for i in range(self.width):
 			for j in range(self.height):
-				print(("*" if self._minesmap[i][j] else self.getnumber(i, j)) if self._revived[i][j] else '+', end='')	
-			print()
+				block = self._map[i][j]
+				if not block.revived():
+					s += 'p' if block.getflag() else '*'
+				elif block.getmine():
+					s += 'M'
+				else:
+					rounding = block.getrounding()
+					s += str(rounding) if rounding else ' '
+			s += '\n'
+		return s[:-1]
 
 	def inner(self):
+		s = ''
 		for i in range(self.width):
 			for j in range(self.height):
-				print(1 if self._minesmap[i][j] else 0, end='')
-			print()
+				block = self._map[i][j]
+				if block.getmine():
+					s += 'M'
+				else:
+					rounding = block.getrounding()
+					s += str(rounding) if rounding else ' '
+			s += '\n'
+		return s[:-1]
+
+if __name__ == '__main__':
+	test = Minesmap(10,10,10)
+	
